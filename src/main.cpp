a5d4039aa619fc773eb81fa6fc51ce4faf498a39
linmo111 <56517417+linmo111@users.noreply.github.com>
2024-02-21 21:20:14 +0800

    day1stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
 
 //Constants
   const uint32_t interval = 100; //Display update interval
@@ -34,6 +35,22 @@
 
 //Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
+const uint32_t AStepSize= (2^32)*440/22000;
+volatile uint32_t currentStepSize;
+
+uint32_t stepSizes [12] = {          };
+void generateStepSize(){
+  for (int i=0;i<11;i++){
+  uint32_t factor= 2^-12;
+  stepSizes[i]=AStepSize*(factor^-9)*(factor^i);
+  }
+}
+void sampleISR() {
+  static uint32_t phaseAcc = 0;
+  phaseAcc += currentStepSize;
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTR_PIN, Vout + 128);
+}
 
 //Function to set outputs using key matrix
 void setOutMuxBit(const uint8_t bitIdx, const bool value) {
@@ -47,10 +64,51 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
 }
 
+
+
+
+std::bitset<4> readCols(){
+  // digitalWrite(RA0_PIN, LOW);
+  // digitalWrite(RA1_PIN, LOW);
+  // digitalWrite(RA2_PIN, LOW);
+  // digitalWrite(REN_PIN, HIGH);
+  
+  std::bitset<4> result;
+  result[0] = digitalRead(C0_PIN);
+  result[1] = digitalRead(C1_PIN);
+  result[2] = digitalRead(C2_PIN);
+  result[3] = digitalRead(C3_PIN);
+  // digitalWrite(REN_PIN, LOW);
+  return result;}
+
+void setRow(uint8_t rowIdx){
+  digitalWrite(REN_PIN, LOW);
+  // uint8_t rowIdxBinary = rowIdx;
+  uint8_t bit0 = rowIdx & 0x01;
+  uint8_t bit1 = rowIdx & 0x02;
+  uint8_t bit2 = rowIdx & 0x04;
+  digitalWrite(RA0_PIN, bit0);
+  digitalWrite(RA1_PIN, bit1);
+  digitalWrite(RA2_PIN, bit2);
+  // std::bitset<4> result=readCols();
+  digitalWrite(REN_PIN, HIGH);
+  
+
+  
+}
+
+
+
+
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
   //Set pin directions
+  generateStepSize();
   pinMode(RA0_PIN, OUTPUT);
   pinMode(RA1_PIN, OUTPUT);
   pinMode(RA2_PIN, OUTPUT);
@@ -66,6 +124,11 @@ void setup() {
   pinMode(C3_PIN, INPUT);
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
+  TIM_TypeDef *Instance = TIM1;
+  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+  sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer->attachInterrupt(sampleISR);
+  sampleTimer->resume();
 
   //Initialise display
   setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
@@ -77,6 +140,7 @@ void setup() {
   //Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+
 }
 
 void loop() {
@@ -85,15 +149,39 @@ void loop() {
   static uint32_t count = 0;
 
   while (millis() < next);  //Wait for next interval
+  
+  // u8g2.setCursor(2,20);
+  uint32_t keypressed;
+  std::bitset<12> inputs;
+  for (int i=0; i<3;i++){
+    setRow(i);
+    delayMicroseconds(3);
+    std::bitset<4> result=readCols();
+    
+    // inputs |= (std::bitset<4>(result.to_ulong()) << 12-4*(i+1)); 
+    for (int j=0; j<4; j++){
+      inputs[11-(i*4+j)]=result[j];
+      if (result[j]==0){
+        keypressed=i*4+j;
+      }
+    }
+    
+  }
+  currentStepSize=stepSizes[keypressed];
+  // analogWrite(OUTR_PIN,  208);
+  
 
   next += interval;
 
   //Update display
   u8g2.clearBuffer();         // clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
+  u8g2.drawStr(2,10,"Helllo World!");// write something to the internal memory
+  // std::bitset<4> inputs = readCols();
+  u8g2.setCursor(2,30);
+  u8g2.print(inputs.to_ulong(),BIN); 
+  
+  // u8g2.print(count++);
   u8g2.sendBuffer();          // transfer internal memory to the display
 
   //Toggle LED
